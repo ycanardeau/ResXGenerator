10db4c6d3996aa133c3da478d57739d65bf1e401
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-07-04 14:23:40 +0900

    Move Windows-only Drawing tests into a separate net8.0-windows project

--- Aigamo.ResXGenerator.Tests/IntegrationTests/TestResxFiles.cs
+++ Aigamo.ResXGenerator.Tests/IntegrationTests/TestResxFiles.cs
@@ -24,9 +24,7 @@ public class TestResxFiles
 		Test1.TextFile.Should().Be("This is a test.\n");
 		Test1.BinaryFile.Should().BeEquivalentTo(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 		Test1.TestIconAsBytes.Should().NotBeNull();
-		Test1.TestIconAsDrawingIcon.Should().NotBeNull().And.BeOfType<System.Drawing.Icon>().Which.Size.Should().Be(new Size(32, 32));
 		Test1.TestImageAsBytes.Should().NotBeNull();
-		Test1.TestImageAsDrawingBitmap.Should().NotBeNull().And.BeOfType<System.Drawing.Bitmap>().Which.Size.Should().Be(new Size(32, 32));
 	}
 	[Fact]
 	public void TestCodeGenResourceGen()
